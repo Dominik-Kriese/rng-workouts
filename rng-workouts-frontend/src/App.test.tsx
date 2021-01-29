0172080c2c8a01018f8b3f2895e0ffba2780f3b2
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2021-01-29 23:01:25 +0100

    Setup styled components and enzyme

--- rng-workouts-frontend/src/App.test.tsx
+++ rng-workouts-frontend/src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import {mount} from "enzyme";
+import 'jest-styled-components';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    const test = mount(<App/>);
+    const test2 = test.find('div');
+    expect(test2).toMatchSnapshot();
 });
--- rng-workouts-frontend/src/App.tsx
+++ rng-workouts-frontend/src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {ThemeProvider} from "styled-components";
+import theme from "./theme/theme";
+import GlobalStyle from "./theme/globalstyles";
+
+const AppContainer = styled.div`
+  background-color: black;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <GlobalStyle/>
+            <AppContainer>
+                test
+            </AppContainer>
+        </ThemeProvider>
+    );
 }
 
 export default App;
