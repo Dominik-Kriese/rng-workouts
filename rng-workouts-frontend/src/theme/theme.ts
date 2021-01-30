ae8344829d5f57d2e27590e5c1f01556e43d4abe
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2021-01-30 12:27:46 +0100

    Introduce proper styling

--- rng-workouts-frontend/src/theme/theme.ts
+++ rng-workouts-frontend/src/theme/theme.ts
@@ -1,8 +1,29 @@
-const theme = {
+export type AppTheme = {
     colors: {
-        background: '#fff',
-        text: '#000'
-    }
+        text: {
+            [key: string]: string
+        },
+        backgrounds: {
+            [key: string]: string
+        }
+    },
+    dropShadows: string
 }
 
-export default theme;
+export const theme: AppTheme = {
+    colors: {
+        text: {
+            defaultBright: 'rgba(255,255,255,0.87)',
+            defaultMedium: 'rgba(255,255,255,0.65)',
+            defaultDull: 'rgba(255,255,255,0.38)',
+            primary: 'rgba(103,170,203,1)'
+        },
+        backgrounds: {
+            defaultLight: 'rgba(45,45,62,1)',
+            defaultDark: 'rgba(7,7,10,1)'
+        }
+    },
+    dropShadows: '-webkit-box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.75);\n' +
+        '-moz-box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.75);\n' +
+        'box-shadow: 0px 0px 4px 0px rgba(0,0,0,0.75);'
+}
